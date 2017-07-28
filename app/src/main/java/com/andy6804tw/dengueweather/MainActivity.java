003a23d78b26e1b11dae5a59590dbefab3507636
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2017-07-28 11:16:17 +0800

    first commit

---
+++ app/src/main/java/com/andy6804tw/dengueweather/MainActivity.java
@@ -0,0 +1,99 @@
+package com.andy6804tw.dengueweather;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.InputStream;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //載入圖片
+        new DownloadImageTask((ImageView) findViewById(R.id.imageView))
+                .execute("http://l.yimg.com/os/mit/media/m/social/images/social_default_logo-1481777.png");
+        init();
+
+    }
+
+    public void init() {
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "http://cloud.culture.tw/frontsite/trans/SearchShowAction.do?method=doFindTypeJ&category=5";
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+
+                        JSONObject jsonObject;
+                        try {
+                            jsonObject = new JSONObject(response);
+                            Log.e("jsonObject",jsonObject.toString());
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getBaseContext(), "無法連接網路!", Toast.LENGTH_SHORT).show();
+                Log.e("Data",error+"");
+            }
+
+        });
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+    }
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+
+        public DownloadImageTask(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            Bitmap mIcon11 = null;
+            try {
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                mIcon11 = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return mIcon11;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            bmImage.setImageBitmap(result);
+        }
+    }
+}
